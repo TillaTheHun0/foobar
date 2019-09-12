377f457f3899b4bd024171c990dc9412a3d2c7a8
Tyler Hall <0tillathehun0@gmail.com>
2019-09-12 14:14:31 -0400

    grants idea

---
+++ grant.js
@@ -0,0 +1,12 @@
+
+const DialogProps = {
+  title: 'string',
+  paragraphText: 'string',
+  buttonText: 'string',
+  date: 'date'
+}
+const Dialog = (
+  <Dialog />
+)
+const DialogLarge = new MuiComponent(Dialog, DialogSchema)
+const DialogLoose = new MuiComponent(Dialog)
