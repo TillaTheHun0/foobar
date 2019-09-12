d6b865cfec2cfdeee0df1bfb11ce756728ad3332
Grant Eagon <grant.eagon@gmail.com>
2019-09-12 14:24:44 -0400

    Update grant.js

--- grant.js
+++ grant.js
@@ -1,12 +1,39 @@
+// versioned api
+import { MuiComponent, MuiComponentSchema } from 'mui-content/0.3';
+
+// default behaviors for property types
+import MuiString from 'mui-content-types/string';
+// versioned type behavior
+import MuiDate from 'mui-content-types/0.2/date';
+
+import spellCheck from 'awesome-spellcheck';
 
 const DialogProps = {
-  title: 'string',
-  paragraphText: 'string',
-  buttonText: 'string',
-  date: 'date'
+    title: MuiString,
+    paragraphText: MuiString,
+    buttonText: MuiString,
+    date: MuiDate
 }
-const Dialog = (
-  <Dialog />
-)
-const DialogLarge = new MuiComponent(Dialog, DialogSchema)
+
+// Prop overrides
+const OtherDialogProps = {
+    ...DialogProps,
+    paragraphText: MuiString({
+        onSave: (content) => spellCheck(content)
+    })
+}
+
+const DialogSchema = new MuiComponentSchema(DialogProps)
+
+const Dialog = (props) => {
+    return <div>
+        <h1>{props.title} <span>{props.date}</span></h1>
+        <p>{props.paragraphText}</p>
+        <button>{props.buttonText}</button>
+    </div>;
+}
+
+const StrictDialog = new MuiComponent(Dialog, DialogSchema)
+const OverrideDialog = new MuiComponent(Dialog, OtherDialogProps)
+// if'n you don't want a schema:
 const DialogLoose = new MuiComponent(Dialog)
